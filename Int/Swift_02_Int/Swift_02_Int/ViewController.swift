ba32308d4eef11c7521980ce9f7ef40dfb29d40c
Neznaika76 <n9032692530@gmail.com>
2023-07-18 00:53:41 +0300

    DZ1

---
+++ Int/Swift_02_Int/Swift_02_Int/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_02_Int
+//
+//  Created by Pavel iPro on 18.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var lable: UIImageView!
+    
+}
+
